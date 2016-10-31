475febd3048035197e63cec26ee18def3abf93ed
pan <pan>
2016-10-31 14:46:14 +0800

    大整数相加

--- php/Gear.php
+++ php/Gear.php
@@ -202,6 +202,30 @@ class Gear
 		return date('Ymd').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
 	}
 
+	/**
+	* 大整数相加
+	* 作者: unknow
+	**/
+	static function strAdd($str1,$str2){
+	    $res = array();
+	    if(strlen($str1) > strlen($str2)){
+	        $str2 = str_pad($str2,strlen($str1),'0',STR_PAD_LEFT);
+	    }
+	    else{
+	        $str1 = str_pad($str1,strlen($str2),'0',STR_PAD_LEFT);
+	    }
+	    for($i = strlen($str1)-1; $i>=0; $i--){
+	        $tmp = $str1[$i] + $str2[$i];
+	        $res[$i] += $tmp;
+	        if($res[$i] >= 10) {
+	            $res[$i] -= 10;
+	            $res[$i-1] += 1;
+	        }
+	    }
+	    ksort($res);
+	    $res = implode('',$res);
+	    return $res;
+	}
 
 
 }
